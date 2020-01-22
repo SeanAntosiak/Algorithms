c04495e375e4813aeceb51480b57bdf8cfc53c4b
seanantosiak <seanantosiak@gmail.com>
2020-01-22 15:30:43 -0500

    updated from local repo

--- recipe_batches/recipe_batches.py
+++ recipe_batches/recipe_batches.py
@@ -2,12 +2,25 @@
 
 import math
 
+
 def recipe_batches(recipe, ingredients):
-  pass 
+    recipe_list = list(recipe.keys())
+    batches = []
+    for item in recipe_list:
+        if item in ingredients:
+            num = ingredients[item] // recipe[item]
+            batches.append(num)
+        else:
+            return(0)
+    lowest_batches = float('inf')
+    for num in batches:
+        if num < lowest_batches:
+            lowest_batches = num
+    return(lowest_batches)
 
 
 if __name__ == '__main__':
-  # Change the entries of these dictionaries to test 
+  # Change the entries of these dictionaries to test
   # your implementation with different inputs
   recipe = { 'milk': 100, 'butter': 50, 'flour': 5 }
   ingredients = { 'milk': 132, 'butter': 48, 'flour': 51 }
